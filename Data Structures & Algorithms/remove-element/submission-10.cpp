e0e60392105a233d5ed499f551337ee65345a712
Viktor Nikolov <viktor.nikolov069@gmail.com>
2026-05-21 18:08:43 +0300

    Add: remove-element - submission-10

---
+++ Data Structures & Algorithms/remove-element/submission-10.cpp
@@ -0,0 +1,17 @@
+class Solution {
+   public:
+    int removeElement(vector<int>& nums, int val) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right];
+                right--;
+            }
+            if (nums[left] != val) {
+                left++;
+            }
+        }
+        return left;
+    }
+};
