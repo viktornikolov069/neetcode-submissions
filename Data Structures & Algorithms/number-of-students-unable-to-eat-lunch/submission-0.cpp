7155fefe508bce45ae00ac6391d8132410cdf5c1
Viktor Nikolov <viktor.nikolov069@gmail.com>
2026-05-30 11:29:40 +0300

    Add: number-of-students-unable-to-eat-lunch - submission-0

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+   public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        std::queue<int> q_stud{students.begin(), students.end()};
+        std::queue<int> q_san{sandwiches.begin(), sandwiches.end()};
+
+        size_t r_count = 0;
+        while (!q_stud.empty() && r_count < q_stud.size()) {
+            if (q_stud.front() == q_san.front()) {
+                q_stud.pop();
+                q_san.pop();
+                r_count = 0;
+            } else {
+                r_count++;
+                q_stud.push(q_stud.front());
+                q_stud.pop();
+            }
+        }
+
+        return q_stud.size();
+    }
+};
+
+// 1 1 1 0 0 1 -> 1 1 0 0 1 -> 1 0 0 1 -> 0 0 1 1 -> 0 1 1 -> 1 1
+// 1 0 0 0 1 1 -> 1 0 0 0 1 -> 1 0 0 0 -> 1 0 0 0 -> 1 0 0 -> 1 0
+
+// 1 1 0 0 -> 1 0 0 1 -> 0 0 1 1 -> 0 1 1 -> 1 1 0 -> 1 0 -> 0 1 -> 1 ->
+// 0 1 0 1 -> 0 1 0 1 -> 0 1 0 1 -> 1 0 1 -> 1 0 1 -> 0 1 -> 0 1 -> 1 ->
