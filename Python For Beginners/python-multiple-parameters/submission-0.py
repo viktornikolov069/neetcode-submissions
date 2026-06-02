efecbb828bd81b1fee573667f1cd8d188fdf781c
Viktor Nikolov <viktor.nikolov069@gmail.com>
2026-06-02 15:20:13 +0300

    Add: python-multiple-parameters - submission-0

---
+++ Python For Beginners/python-multiple-parameters/submission-0.py
@@ -0,0 +1,13 @@
+def two_sum(var1, var2):
+    print(var1 + var2)
+
+
+def three_sum(var1, var2, var3):
+    print(var1 + var2 + var3)
+
+
+two_sum(7, 10)
+three_sum(3, 5, 8)
+# do not modify below this line
+two_sum(10, 9)
+three_sum(5, 14, 6)
