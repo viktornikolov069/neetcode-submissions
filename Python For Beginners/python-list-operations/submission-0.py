17705142a4af7d7b8d93f269804f8d1ac66afd67
Viktor Nikolov <viktor.nikolov069@gmail.com>
2026-06-02 22:04:07 +0300

    Add: python-list-operations - submission-0

---
+++ Python For Beginners/python-list-operations/submission-0.py
@@ -0,0 +1,17 @@
+def check_list_empty(my_list) -> bool:
+    return False if my_list else True
+
+
+def check_element_in_list(my_list, element) -> bool:
+    return True if element in my_list else False
+
+
+# do not modify below this line
+print(check_list_empty([]))
+print(check_list_empty([1, 2, 3]))
+
+print(check_element_in_list([1, 2, 3], 1))
+print(check_element_in_list([1, 2, 3], 4))
+
+print(check_element_in_list(["Apple", "Banana", "Orange"], "Banana"))
+print(check_element_in_list(["Apple", "Banana", "Orange"], "Grape"))
