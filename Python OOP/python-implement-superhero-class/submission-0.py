b8065f5745e4eff29f7489db2d188036d7764179
Viktor Nikolov <viktor.nikolov069@gmail.com>
2026-06-03 20:54:25 +0300

    Add: python-implement-superhero-class - submission-0

---
+++ Python OOP/python-implement-superhero-class/submission-0.py
@@ -0,0 +1,28 @@
+class SuperHero:
+    """
+    A class to represent a superhero.
+
+    Attributes:
+        name (str): The superhero's name
+        power (str): The superhero's main superpower
+        health (int): The superhero's health points
+    """
+
+    def __init__(self, name: str, power: str, health: int):
+        self.name = name
+        self.power = power
+        self.health = health
+    
+    def print_attributes(self):
+        print(self.name)
+        print(self.power)
+        print(self.health)
+
+
+# TODO: Create Superhero instances
+batman = SuperHero("Batman", "Intelligence", 100)
+superman = SuperHero("Superman", "Strength", 150)
+
+# TODO: Print out the attributes of each superhero
+batman.print_attributes()
+superman.print_attributes()
