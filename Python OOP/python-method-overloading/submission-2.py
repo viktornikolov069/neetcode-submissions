532bd4eca79e7912809a1424f66a136aaece4129
Viktor Nikolov <viktor.nikolov069@gmail.com>
2026-06-14 18:54:21 +0300

    Add: python-method-overloading - submission-2

---
+++ Python OOP/python-method-overloading/submission-2.py
@@ -0,0 +1,15 @@
+class TextProcessor:
+    # Implement method overloading for format_text method
+    def format_text(self, text1: str, text2: str = None) -> str:
+        res = ""
+        if text2 is None:
+            res = text1.upper()
+        else:
+            res = text1 + text2
+        return res
+
+
+# Don't modify the code below
+processor = TextProcessor()
+print(processor.format_text("hello"))
+print(processor.format_text("hello", "world"))
